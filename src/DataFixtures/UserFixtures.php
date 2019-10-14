378457f7eec3176d540a1919189b6287e4136c89
goudvir <goudvir@ndlp.fr>
2019-10-14 12:53:45 +0200

    [Core] Add users management #2

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('user@gmail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword('123456');
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('admin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('123456');
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('superadmin@gmail.com');
+        $user->setRoles(['ROLE_SUPER_ADMIN']);
+        $user->setPassword('123456');
+        $manager->persist($user);
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'the_new_password'
+        ));
+
+        $manager->flush();
+    }
+}
